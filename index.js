e89dd2e0a8703f508d8dc5f09747b53a74005a50
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-05-09 19:22:03 +0600

    All required lib added

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const res = require('express/lib/response');
-require('dotenv').config();
-const port = process.env.PORT || 5000;
+
 
 const app = express();
 
